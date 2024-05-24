c3dbf228fe18355b54ea4cb74362a4d1c8c7cb61
samuelhodges-adi <153766026+samuelhodges-adi@users.noreply.github.com>
2024-05-24 12:03:26 -0400

    Update init.bat

--- init.bat
+++ init.bat
@@ -1,11 +1,11 @@
-@echo off
+@REM echo off
 echo copying user settings
 xcopy "%programdata%\Remote Eval Agent\UserInit\AppData\" "%appdata%\..\" /E/H/Y
 xcopy "%programdata%\Remote Eval Agent\UserInit\UserProfile\" "%userprofile%\" /E/H/Y
 timeout 8
 
 start "" "C:\Program Files\Analog Devices\ACE\ACE.exe"
-start "" C:\Python311\pythonw.exe "%~dp0\init.py"
+start "" C:\Python311\pythonw.exe "%~dp0init.py"
 
 timeout 10
 
