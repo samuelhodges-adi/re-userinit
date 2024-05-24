c46f1fee740d1339e9d06f67ab6b587188b9e281
samuelhodges-adi <153766026+samuelhodges-adi@users.noreply.github.com>
2024-05-24 11:29:54 -0400

    Create init.bat

---
+++ init.bat
@@ -0,0 +1,12 @@
+@echo off
+echo copying user settings
+xcopy "%programdata%\Remote Eval Agent\UserInit\AppData\" "%appdata%\..\" /E/H/Y
+xcopy "%programdata%\Remote Eval Agent\UserInit\UserProfile\" "%userprofile%\" /E/H/Y
+timeout 8
+
+start "" "C:\Program Files\Analog Devices\ACE\ACE.exe"
+start "" C:\Python311\pythonw.exe "C:\ProgramData\Remote Eval Agent\userinit.py"
+
+timeout 10
+
+exit
