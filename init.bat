a9522dfc13ad4a9b736110fe1c353299c5da0163
samuelhodges-adi <153766026+samuelhodges-adi@users.noreply.github.com>
2024-06-12 16:23:20 -0400

    Update init.bat

--- init.bat
+++ init.bat
@@ -1,12 +1,18 @@
 @REM echo off
-echo copying user settings
-xcopy "%programdata%\Remote Eval Agent\UserInit\AppData\" "%appdata%\..\" /E/H/Y
-xcopy "%programdata%\Remote Eval Agent\UserInit\UserProfile\" "%userprofile%\" /E/H/Y
-timeout 8
 
-start "" "C:\Program Files\Analog Devices\ACE\ACE.exe"
-call "%~dp0run-python-init.bat"
+if "%1"=="chrome" (
+  echo Launching Chrome
+  call "%~dp0run-python-init.bat"
+) else (
+  echo Performing first time setup
+  echo copying user settings
+  xcopy "%programdata%\Remote Eval Agent\UserInit\AppData\" "%appdata%\..\" /E/H/Y
+  xcopy "%programdata%\Remote Eval Agent\UserInit\UserProfile\" "%userprofile%\" /E/H/Y
+  timeout 8
 
-timeout 10
+  start "" "C:\Program Files\Analog Devices\ACE\ACE.exe"
+  call "%~dp0run-python-init.bat"
+  timeout 10
+)
 
 exit
