2112fbab001c096d65b8206586f42b61cd674e57
samuelhodges-adi <153766026+samuelhodges-adi@users.noreply.github.com>
2024-08-20 17:08:17 -0400

    Update init.bat

--- init.bat
+++ init.bat
@@ -9,7 +9,7 @@ if "%1"=="chrome" (
   xcopy "%programdata%\Remote Eval Agent\UserInit\AppData\" "%appdata%\..\" /E/H/Y
   xcopy "%programdata%\Remote Eval Agent\UserInit\UserProfile\" "%userprofile%\" /E/H/Y
   echo Starting local HTTP server
-  start "HTTP Server" python -m http.server 8000
+  start /B python -m http.server 8000
   timeout 8
 
   start "" "C:\Program Files\Analog Devices\ACE\ACE.exe"
