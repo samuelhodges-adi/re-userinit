8745c17a0b7115a04ab8afb677afbc20905d6abd
samuelhodges-adi <153766026+samuelhodges-adi@users.noreply.github.com>
2025-07-18 11:18:50 -0400

    Update run-python-init.bat

--- run-python-init.bat
+++ run-python-init.bat
@@ -1,2 +1,2 @@
 taskkill /F /IM chrome.exe
-start "" /B cmd /c ""C:\Python311\python.exe" "%~dp0init.py" >> "%USERPROFILE%\Documents\python_init_log.txt" 2>&1
+start "" /B cmd /c C:\Python311\python.exe -u "C:\Users\Guest14474\re_initialize\init.py" >> "C:\Users\Guest14474\Documents\python_init_log.txt" 2>&1
