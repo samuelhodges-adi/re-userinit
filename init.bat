68900b1e1c866d3c2b243c5017e9e20f35c02f5d
samuelhodges-adi <153766026+samuelhodges-adi@users.noreply.github.com>
2025-07-23 11:39:02 -0400

    Update init.bat

--- init.bat
+++ init.bat
@@ -4,18 +4,19 @@ if "%1"=="chrome" (
   echo Launching Chrome
   call "%~dp0run-python-init.bat"
 ) else (
-  echo Performing first time setup
-  echo Copying user settings...
-
-  robocopy "%programdata%\Remote Eval Agent\UserInit\AppData" "%appdata%\.." /E /COPYALL /R:2 /W:1
-  robocopy "%programdata%\Remote Eval Agent\UserInit\UserProfile" "%userprofile%" /E /COPYALL /R:2 /W:1
-
-  taskkill /F /IM ACE.exe
-  timeout /t 8 /nobreak >nul
-
-  start "" "C:\Program Files\Analog Devices\ACE\ACE.exe"
-  call "%~dp0run-python-init.bat"
+  echo Performing first time setup  
+  echo Copying user settings...  
+  
+  robocopy "C:\ProgramData\Remote Eval Agent\UserInit\AppData" "%appdata%\.." /E /COPY:DATSO /R:2 /W:1  
+  robocopy "C:\ProgramData\Remote Eval Agent\UserInit\UserProfile" "%userprofile%" /E /COPY:DATSO /R:2 /W:1  
+  
+  taskkill /F /IM ACE.exe  
+  timeout 8 
+  
+  start "" "C:\Program Files\Analog Devices\ACE\ACE.exe"  
+  call "%~dp0run-python-init.bat"  
   timeout 10
+
 )
 
 exit /b
