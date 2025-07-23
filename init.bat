d3aabb9ed21668c369807b042e02cc1561d4c0ff
samuelhodges-adi <153766026+samuelhodges-adi@users.noreply.github.com>
2025-07-23 11:57:29 -0400

    Update init.bat

--- init.bat
+++ init.bat
@@ -5,18 +5,23 @@ if "%1"=="chrome" (
   call "%~dp0run-python-init.bat"
 ) else (
   echo Performing first time setup  
+  echo Deleting existing user Desktop and Chrome data...
+
+  rmdir /S /Q "%userprofile%\Desktop"
+  rmdir /S /Q "%localappdata%\Google\Chrome"
+
   echo Copying user settings...  
   
   robocopy "C:\ProgramData\Remote Eval Agent\UserInit\AppData" "%appdata%\.." /E /COPY:DATSO /R:2 /W:1  
   robocopy "C:\ProgramData\Remote Eval Agent\UserInit\UserProfile" "%userprofile%" /E /COPY:DATSO /R:2 /W:1  
   
   taskkill /F /IM ACE.exe  
-  timeout 8 
-  
+  timeout /t 8 /nobreak >nul
+
   start "" "C:\Program Files\Analog Devices\ACE\ACE.exe"  
   call "%~dp0run-python-init.bat"  
-  timeout 10
-
+  timeout /t 10 /nobreak >nul
 )
 
 exit /b
+
